855e8608770b541dd8fe7bd426deed8b3b8ac92b
natamartins <nata.codedev@gmai.com>
2023-05-12 10:31:12 -0300

    new update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import * as Scroll from 'react-scroll'
 let Button = Scroll.Link
 import AOS from 'aos';
 import 'aos/dist/aos.css'
-
+// 
 import { MessageCircleIcon, ChevronDown, Instagram, Linkedin, Menu, Send, Twitter } from "lucide-react"
 import CardList from "@/components/CardList"
 import Image from "next/image"
-
+// 
 import Imagem01 from '../../public/img/3d.svg'
 import Photo03 from '../../public/img/Rocket.gif'
 import Photo04 from '../../public/img//Futuristic.gif'
@@ -199,6 +199,7 @@ export default function Home(): JSX.Element {
               <p>Comece um projeto conosco</p>
             </div>
           </div>
+
           <Form />
         </section>
         <div className="card_footer-contact">
